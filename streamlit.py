473ef459ec616ab989e56ed74b1dbe5f68e04a3c
Gina Tebaldi <gina.tebaldi@stonybrook.edu>
2021-12-16 15:30:16 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -8,6 +8,24 @@ This is a temporary script file.
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import time
+import matplotlib.pyplot as plt
 
 st.title('HHA 504 Streamlit Final Assignment')
+
+@st.cache
+def load_hospitals():
+    hospitaldf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_hospital.csv')
+    return hospitaldf
+@st.cache
+def load_Outpatient():
+    doutpatientdf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_outpatient.csv')
+    return 
+
+@st.cache
+def load_Inpatient():
+    inpatientdf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_inpatient.csv')
+    return inpatientdf
