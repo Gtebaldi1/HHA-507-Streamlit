7d5a0dc49dfd296508d982a013a2c19ced1b8025
Gina Tebaldi <gina.tebaldi@stonybrook.edu>
2021-12-16 15:32:34 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -8,11 +8,6 @@ This is a temporary script file.
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import time
-import matplotlib.pyplot as plt
 
 st.title('HHA 504 Streamlit Final Assignment')
 
