1b69295ae477a9f9f7a9e119bbde05fec00116d0
Gina Tebaldi <gina.tebaldi@stonybrook.edu>
2021-12-16 17:44:15 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -14,11 +14,11 @@ st.title('HHA 504 Streamlit Final Assignment')
 st.write('Gina Tebaldi ') 
 
 @st.cache
-def load_hospitals():
+def load_hospital():
    hospitaldf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
    return hospitaldf
 @st.cache
-def load_inatpatient():
+def load_inpatient():
     inpatientdf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv')
     return inpatientdf
 @st.cache
@@ -27,7 +27,7 @@ def load_outpatient():
     return outpatientdf
 
 hospitaldf = load_hospital()
-inpatientdf = load_inatpatient()
+inpatientdf = load_inpatient()
 outpatientdf = load_outpatient()  
 
 st.header('Question 1: How does Stony Brook compare to the rest of New York?')
