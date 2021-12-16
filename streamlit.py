3e3d18975d9b014adc9a4ffd74053358d8853416
Gina Tebaldi <gina.tebaldi@stonybrook.edu>
2021-12-16 15:14:54 -0500

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import streamline as st
+import pandas as pd
+import numpy as np
+
+
+st.title('HHA 504 Streamlit Final Assignment')
