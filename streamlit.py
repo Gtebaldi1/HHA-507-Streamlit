313a79eedcd5b1f3ee8596f2e035f2a7fa626b3f
Gina Tebaldi <gina.tebaldi@stonybrook.edu>
2021-12-16 21:26:37 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -62,7 +62,7 @@ st.dataframe(sbinpatient)
 sbdischarges = sbinpatient.pivot_table(index =['drg_definition'],values =['total_discharges'],aggfunc='mean')
 st.header(' Discharges for DRG Codes at Stony Brook')
 st.dataframe(sbdischarges)
-st.markdown('Scrolling through the pivot table the highest amount of discharges came from "SEPTICEMIA OR SEVERE SEPSIS W/O MV 96+ HOURS W MCC"')
+st.markdown('Scrolling through the pivot table the highest amount of discharges came from "SEPTICEMIA OR SEVERE SEPSIS W/O MV 96+ HOURS W MCC" 628 discharges, followed by "MAJOR JOINT REPLACEMENT OR REATTACHMENT OF LOWER EXTREMITY W/O MCC".')
 
 
 
